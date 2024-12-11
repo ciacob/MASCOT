de34e47b4ae90bbb4823fba463b8577b2686c2ca
Claudius Tiberiu Iacob <claudius.iacob@gmail.com>
2024-12-11 14:46:18 +0200

    updated test code

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ function isWin() {
 }
 
 // MAIN
-const workspace_dir = isWin
+const workspace_dir = isWin()
   ? "d:\\_DEV_\\github\\actionscript"
   : "/Users/ciacob/_DEV_/github/actionscript";
 
-const cache_dir = isWin
+const cache_dir = isWin()
   ? "d:\\_DEV_\\github\\nodejs\\MASCOT\\cache"
   : "/Users/ciacob/_DEV_/github/node_js/MASCOT/cache";
-  
+
 const userName = "ciacob";
 const repoLanguages = ["ActionScript"];
-const dry_run_mode = false;
+const dry_run_mode = true;
 (async function () {
   const report = await cloneRepos(
     workspace_dir,
@@ -27,7 +27,8 @@ const dry_run_mode = false;
     dry_run_mode
   );
   console.dir(report, { depth: null });
-
-  doShallowScan(workspace_dir, cache_dir, true);
-  doDeepScan(workspace_dir, cache_dir, true);
+  if (!dry_run_mode) {
+    doShallowScan(workspace_dir, cache_dir, true);
+    doDeepScan(workspace_dir, cache_dir, true);
+  }
 })();
