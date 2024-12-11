4509803433e2039a8d0aee2b3c03745c9f84c7f2
Claudius Tiberiu Iacob <claudius.iacob@gmail.com>
2024-12-11 14:39:01 +0200

    OS based testing paths provided
    Added dynamic provisioning of test paths.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-const { cloneRepos, cloneRepo } = require ('./own_modules/clone_tools');
-const { doShallowScan, doDeepScan } = require ('./own_modules/scan_tools');
+const { cloneRepos, cloneRepo } = require("./own_modules/clone_tools");
+const { doShallowScan, doDeepScan } = require("./own_modules/scan_tools");
+const os = require("os");
 
+function isWin() {
+  return os.platform() === "win32";
+}
 
 // MAIN
-const workspace_dir = "d:\\_DEV_\\github\\actionscript";
-const cache_dir = "d:\\_DEV_\\github\\nodejs\\MASCOT\\cache";
+const workspace_dir = isWin
+  ? "d:\\_DEV_\\github\\actionscript"
+  : "/Users/ciacob/_DEV_/github/actionscript";
+
+const cache_dir = isWin
+  ? "d:\\_DEV_\\github\\nodejs\\MASCOT\\cache"
+  : "/Users/ciacob/_DEV_/github/node_js/MASCOT/cache";
+  
 const userName = "ciacob";
 const repoLanguages = ["ActionScript"];
-const dry_run_mode = true;
+const dry_run_mode = false;
 (async function () {
   const report = await cloneRepos(
     workspace_dir,
@@ -16,7 +26,7 @@ const dry_run_mode = true;
     undefined,
     dry_run_mode
   );
-  console.dir(report, { depth : null});
+  console.dir(report, { depth: null });
 
   doShallowScan(workspace_dir, cache_dir, true);
   doDeepScan(workspace_dir, cache_dir, true);
